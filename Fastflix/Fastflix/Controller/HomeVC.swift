5079905516eaa4497792ec7ce460f23e06624ee7
Jeon-heaji <heajijeon@gmail.com>
2019-07-10 17:33:21 +0900

    docs: deby - reupload basic Project

---
+++ Fastflix/Fastflix/Controller/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  Fastflix
+//
+//  Created by hyeoktae kwon on 2019/07/10.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
